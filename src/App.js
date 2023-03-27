b411f1c76c1d1c3b4114a4285837a798c082816b
阿橘 <liuyongly9527@gmail.com>
2023-03-27 16:58:20 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="ai_chat">
+      <div className="ai_chat_nav"></div>
+      <div className="ai_chat_main"></div>
+    </div>
+  );
+}
+
+export default App;
